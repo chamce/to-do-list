9c4b7704488ebb25b9989781eb75676a1b68a309
chamce <whitakerchancellor@yahoo.com>
2021-03-24 17:10:21 -0400

    wonky checkmarks and no local storage

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     let temp = this.state.list;
     for (let item of temp) {
       if (item.time === time) {
-        item.status = false;
+        item.status = !item.status;
       }
     }
     this.setState({
