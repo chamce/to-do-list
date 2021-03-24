15964102240d0fb31d2f0a875d95dcad2de73b1c
chamce <whitakerchancellor@yahoo.com>
2021-03-24 17:01:29 -0400

    doesn't have local storage and cannot uncheck item

--- src/App.js
+++ src/App.js
@@ -1,71 +1,122 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import InputBar from './InputBar.js';
-import List from './List.js';
-import FilterBar from './FilterBar.js';
+import Item from './Item.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       input: '',
-      filter: '',
-      list: [{ id: 1, text: "Something", status: true },
-      { id: 2, text: "Something else", status: true }]
+      filter: 'All',
+      list: []
     }
+    this.textEntered = this.textEntered.bind(this);
+    this.addClicked = this.addClicked.bind(this);
+    this.checkmarkClicked = this.checkmarkClicked.bind(this);
+    this.deleteClicked = this.deleteClicked.bind(this);
+    this.filterClicked = this.filterClicked.bind(this);
+    this.clearClicked = this.clearClicked.bind(this);
+    this.listMapper = this.listMapper.bind(this);
+    this.listFilterer = this.listFilterer.bind(this);
   }
 
-  submitClicked() {
-    this.addListItem();
+  addClicked() {
+    // add list item
+    let item = { text: this.state.input, time: Date.now(), status: true };
+    this.setState({
+      list: this.state.list.concat(item),
+      input: ''
+    });
   }
 
-  checkClicked() {
-    this.checkItem();
+  checkmarkClicked(time) {
+    // checkmark item
+    let temp = this.state.list;
+    for (let item of temp) {
+      if (item.time === time) {
+        item.status = false;
+      }
+    }
+    this.setState({
+      list: temp
+    });
   }
 
-  removeClicked() {
-    this.removeItem();
+  deleteClicked(time) {
+    // delete item
+    this.setState({
+      list: this.state.list.filter(item => item.time !== time)
+    });
   }
 
-  filterClicked() {
-    this.changeFilter();
+  filterClicked(newFilter) {
+    // change filter
+    this.setState({
+      filter: newFilter
+    });
   }
 
   clearClicked() {
-    this.clearCompleted();
-  }
-
-  addListItem() {
+    // clear completed
 
   }
 
-  checkItem() {
-
+  listMapper(item, index) {
+    console.log(item, index);
+    return <Item item={item} key={index} delete={this.deleteClicked} checkmark={this.checkmarkClicked}></Item>;
   }
 
-  removeItem() {
-
+  listFilterer(item) {
+    if (this.state.filter === 'All') {
+      return item;
+    }
+    else if (this.state.filter === 'Active' && item.status) {
+      return item;
+    }
+    else if (this.state.filter === 'Completed' && !item.status) {
+      return item;
+    }
   }
 
-  changeFilter() {
-
-  }
-
-  clearCompleted() {
-
+  textEntered(event) {
+    this.setState({ input: event.target.value });
+    console.log(this.state.input);
   }
 
   render() {
+
     return (
       <div className='container text-center'>
         <div className='row'>
           <div className='col-md-4 offset-md-4 col-sm-12'>
-            <InputBar></InputBar>
-            <FilterBar></FilterBar>
-            <List
-              todos={this.state.list}
-            ></List>
+            <>
+              <div className='row'>
+                <h3>What needs to be done?</h3>
+              </div>
+              <div className='row mb-2'>
+                <input type="text" name="name" value={this.state.input} onChange={this.textEntered} />
+              </div>
+              <div className='row mb-2'>
+                <button className='bg-dark text-white' onClick={this.addClicked}>Add</button>
+              </div>
+            </>
+            <>
+              <div className='row mb-1'>
+                <div className="btn-group" role="group" aria-label="Basic example">
+                  <button type="button" className="btn btn-primary" onClick={() => this.filterClicked('All')}>All</button>
+                  <button type="button" className="btn btn-primary" onClick={() => this.filterClicked('Active')}>Active</button>
+                  <button type="button" className="btn btn-primary" onClick={() => this.filterClicked('Completed')}>Completed</button>
+                </div>
+              </div>
+              <div className='row text-start'>
+                <h6>{this.state.list.filter(item => item.status === true).length} active tasks</h6>
+              </div>
+            </>
+            <>
+              <div className='row mb-2'>
+                {this.state.list.filter(this.listFilterer).map(this.listMapper)}
+              </div>
+            </>
           </div>
         </div>
       </div>
